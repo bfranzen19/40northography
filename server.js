60d69df557be891ca6780241601ed7970e96682e
bt <25850024+bfranzen19@users.noreply.github.com>
2019-07-12 09:24:57 -0600

    updating server.js and removing mapseachtool functionality

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(express.static('./public'))
 /* map search tool variables */
 
 /* allows api keys to be hidden & accesses the api key */
-const dotenv = require('dotenv').config();
-const key = process.env.key1
+// const dotenv = require('dotenv').config();
+// const key = process.env.key1
 
 
 /* routes */
@@ -114,52 +114,52 @@ app.get('/walkingMap', function(req,res) {
 /* map search tool routes */
 
 /* sends the index.html file from the '/' route */
-app.get('/MapSearchTool', function(req, res) {
-    res.sendFile('./public/MapSearchTool/public/html/map_index.html', {root:'./'});
-});
+// app.get('/MapSearchTool', function(req, res) {
+//     res.sendFile('./public/MapSearchTool/public/html/map_index.html', {root:'./'});
+// });
 
 /* makes the api call to google maps */
-app.post('/searchIt', function(req,res) {
-    let totalUserRatings, ratings;
+// app.post('/searchIt', function(req,res) {
+//     let totalUserRatings, ratings;
 
-    /* builds the query string*/
-    let query = req.body.toDo + ' ' + req.body.placeInput
+//     /* builds the query string*/
+//     let query = req.body.toDo + ' ' + req.body.placeInput
 
-    request({
-        url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&rankby=prominence&key=${key}`
-    },
-    function(error, response, body) {
-        if(error || (response.statusCode !== 200)) {
-            console.log(`here's the error: `, error);
-            console.log(`here's the status code: `, response.statusCode);
-            res.send(`oops, something went wrong`)
-        } else {
-            let bod = JSON.parse(body)
-            let top20 = Array.from(bod.results);
+//     request({
+//         url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${query}&rankby=prominence&key=${key}`
+//     },
+//     function(error, response, body) {
+//         if(error || (response.statusCode !== 200)) {
+//             console.log(`here's the error: `, error);
+//             console.log(`here's the status code: `, response.statusCode);
+//             res.send(`oops, something went wrong`)
+//         } else {
+//             let bod = JSON.parse(body)
+//             let top20 = Array.from(bod.results);
 
-            /* sort by rating and total number of user ratings */
-            for(let i=0 ; i<top20.length ; i++) {
-                /* sorts by the number of total user ratings */
-                totalUserRatings = top20.sort((a,b) => b.user_ratings_total-a.user_ratings_total)
-                top20.sort((a,b) => b.rating-a.rating)
+//             /* sort by rating and total number of user ratings */
+//             for(let i=0 ; i<top20.length ; i++) {
+//                 /* sorts by the number of total user ratings */
+//                 totalUserRatings = top20.sort((a,b) => b.user_ratings_total-a.user_ratings_total)
+//                 top20.sort((a,b) => b.rating-a.rating)
 
-                /* sorts by the highest rating to return the highest and most rated in the search area */
-                ratings =
-                totalUserRatings.sort((a,b) => b.rating-a.rating)
+//                 /* sorts by the highest rating to return the highest and most rated in the search area */
+//                 ratings =
+//                 totalUserRatings.sort((a,b) => b.rating-a.rating)
 
-                /* sorts by the cheapest rating. for personal travel research. */
-                // cheapest = ratings.sort((a,b) => a.price_level - b.price_level)
-            }
+//                 /* sorts by the cheapest rating. for personal travel research. */
+//                 // cheapest = ratings.sort((a,b) => a.price_level - b.price_level)
+//             }
 
-            /* send the sorted top 20 locations to the front end */
-            res.send(ratings)
+//             /* send the sorted top 20 locations to the front end */
+//             res.send(ratings)
 
-            /* adding cheapest for the cheapest, highest, and most rated bar in any area. for personal travel research. */
-            // res.send(cheapest)
+//             /* adding cheapest for the cheapest, highest, and most rated bar in any area. for personal travel research. */
+//             // res.send(cheapest)
 
-        }
-    });
-})
+//         }
+//     });
+// })
 
 
 
